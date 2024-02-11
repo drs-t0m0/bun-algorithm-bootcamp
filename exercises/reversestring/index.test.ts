b077b7902eaacc53a92fed32b1d4160cdae38c52
drs-t0m0 <drs-tomo@hotmail.co.jp>
2024-02-11 21:09:55 +0900

    add reversestring

---
+++ exercises/reversestring/index.test.ts
@@ -0,0 +1,14 @@
+import {reverse} from "./index.ts";
+import {expect, test} from "bun:test";
+
+test("Reverse function exists", () => {
+  expect(reverse).toBeDefined();
+});
+
+test("Reverse reverses a string", () => {
+  expect(reverse("abcd")).toEqual("dcba");
+});
+
+test("Reverse reverses a string", () => {
+  expect(reverse("  abcd")).toEqual("dcba  ");
+});
